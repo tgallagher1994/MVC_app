124db50ed6404a23412581bde241284c4d12e167
Travis <shance28@live.com>
2025-04-29 22:26:54 -0400

    task app CRUD

---
+++ webapp_MVC/Models/TaskItem.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace webapp_MVC.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IsComplete { get; set; }
+    }
+}
